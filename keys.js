4e385ef058e2c42086a75d85f6f3e2219bf19f41
Meg O'Roark <msoroark@gmail.com>
2017-09-02 23:30:05 -0500

    Twitter debug/spotify imdb completed

---
+++ keys.js
@@ -0,0 +1,10 @@
+
+
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'slT0rCBul08VCDkn2u0Om1fGp',
+  consumer_secret: 'ocs9huAIkevbqyfgdiIkwcRgHSrgLZqSv9JPuDhlDYi18la3q1',
+  access_token_key: '75947261-2e1c7WY9pOicniGAoUGUWEWVgSqUgrIkUMnoStw2i',
+  access_token_secret: 'lkBx6Oe5sXCKpAfdr2qCnsnskrkLHdbD6RieKYOAGTM9R',
+}
---
+++ liri.js
@@ -0,0 +1,61 @@
+var key = require('./keys.js');
+var twitterAPI = require('twitter');
+var twitter = new twitterAPI(key);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify({
+    id: '0a96c37f30e64742b3cbc6a6613ec197',
+    secret: '926c8f8c5ba34df3adaff7cd2527773f'
+});
+var request = require('request');
+var fs = require('fs');
+
+
+
+var command = (process.argv[2]);
+
+if (command === "my-tweets") {
+
+    console.dir(twitter);
+    twitter.get('favorites/list', function(error, tweets, response) {
+        if(error) {
+            console.dir(error);
+        }
+        console.dir(tweets); // The favorites. 
+  
+    });
+
+
+
+} else if (command === "spotify-this-song") {
+
+    var song = (process.argv[3]);
+
+    spotify
+        .search({ type: 'track', query: song })
+        .then(function(response) {
+            console.log("Artists: " + response.tracks.items[0].album.artists[0].name);
+            console.log("Song: " + response.tracks.items[0].name);
+            console.log("Preview: " + response.tracks.items[0].album.artists[0].external_urls.spotify);
+            console.log("Album: " + response.tracks.items[0].album.name);
+        })
+        .catch(function(err) {
+            console.log(err);
+        });
+
+
+
+} else if (command === "movie-this") {
+    var movie = (process.argv[3]);
+    if (!movie) {
+        movie = "Mr. Nobody";
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=40e9cece";
+    request(queryUrl, function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            console.log("Title: " + JSON.parse(body).Title + "\nRealease: " + JSON.parse(body).Released +
+                "\nRating: " + JSON.parse(body).imdbRating + "\nRotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value + "\nCountry: " + JSON.parse(body).Country + "\nLanguage: " + JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors)
+        }
+    });
+}
